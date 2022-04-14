61435f4780003547f1560411ffc5c2adcada7669
Lesha Shvetsov <lexot.dev@gmail.com>
2022-04-14 18:20:01 +0500

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,9 @@
-const Modal =({ message, isOpen, onClose, children })=> {
+const Modal = ({ id, title, imageUrl, description, rating, children }) => {
     if (!isOpen) return null
-    return ReactDOM.createPortal(    
-      <div className="modal">
-        <span className="message">{message}</span>
-        <button onClick={onClose}>Close</button>
-      </div>,
-      domNode)
-  }
+    return ReactDOM.createPortal(
+        <div className="modal">
+            <span className="message">{message}</span>
+            <button onClick={onClose}>Close</button>
+        </div>,
+        domNode)
+}

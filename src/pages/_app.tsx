d940c0bcf4a5eab8387daca6c7a15630fbb56bab
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-04-15 15:31:39 +0800

    default dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import theme from '../theme'
+import theme from '../styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SWRConfig } from 'swr'
 import fetcher from '~/libs/fetcher'
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,20 +1,25 @@
 import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = {
+  mono: `'Menlo', monospace`
+}
 
 const breakpoints = createBreakpoints({
   sm: '40em',
   md: '52em',
   lg: '64em',
-  xl: '80em',
+  xl: '80em'
 })
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+}
+
 const theme = extendTheme({
-  colors: {
-    black: '#16161D',
-  },
   fonts,
+  config,
   breakpoints,
 })
 
